63bce59d1842d8a9ca04ba38dca6ffc81b1a0ec9
brothernk <brothernk21@hotmail.com>
2018-03-14 10:40:41 -0500

    added some basic starting code

---
+++ routes/api-routes.js
@@ -0,0 +1,54 @@
+//Will be able to add more once we know what we're making slash
+//what our database is going to look like. The equal signs are 
+//placeholders.
+
+//Dependencies
+const db = require("../models");
+
+//Routing
+module.exports = function(app){
+
+	//GET Routing
+	app.get("/api/=========", function(request, response){
+
+		//Add code for whatever it is we want to get.
+
+	});
+
+	//POST Routing --------- MAY CHANGE "BODY" BASED ON DB
+	app.post("/api/========", function(request, response){
+
+		db.Post.create(request.body).then(function(dbPost){
+			response.json(dbPost);
+		});
+
+	});
+
+	//DELETE Routing
+	app.delete("/api/========", function(request, response){
+
+		db.Post.destroy({
+			where: {
+				id:request.params.id
+			}
+		})
+		.then(function(dbPost){
+			response.json(dbPost);
+		});
+	});
+
+	//PUT Routing --------- MAY CHANGE "BODY" BASED ON DB
+	app.put("/api/===========", function(request, response){
+		
+		db.Post.update(
+			request.body,
+			{
+				where:{
+				id:request.body.id
+				}
+			})
+			.then(function(dbPost){
+				response.json(dbPost);
+			});
+	});
+};
