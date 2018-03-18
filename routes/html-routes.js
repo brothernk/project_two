b569cbcf58504bcdb94e2dbafa9bfc8b48b9c82d
jstudenski <jstudenski@gmail.com>
2018-03-17 22:44:05 -0500

    handlebars displaying properly

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,8 +4,20 @@ const path = require("path");
 //Routing
 module.exports = function(app){
 
-	app.get("/", function(request, response){
-		response.sendFile(path.join(__dirname, "../public/index.html"));
+	app.get("/", function(req, res){
+
+    res.render("index");
+
+
+		// response.sendFile(path.join(__dirname, "../public/index.html"));
 	});
 	
 };
+
+
+
+// // Redirect to index
+// router.get('/', function (req, res) {
+//   res.redirect('/index');
+// });
+
