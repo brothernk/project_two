5e2b075a4ff9c151dd0c47733d85b6b3a0b47f1d
brothernk <brothernk21@hotmail.com>
2018-03-26 22:47:18 -0500

    categories

--- public/assets/js/post.js
+++ public/assets/js/post.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
   //Function to get categories
   function getCategories() {
     $.get("/api/categories", renderCategoryList);
-
   }
 
   //Render list of categories
